65107303e741f3f4202b6ee34681cb1006594a5a
Martin Bebey <bebeymartin@gmail.com>
2018-09-28 16:52:12 -0400

    debugging

---
+++ app/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+var platform = platform_browser_dynamic_1.platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
+//# sourceMappingURL=main.js.map
